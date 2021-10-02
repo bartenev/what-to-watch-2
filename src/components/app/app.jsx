7bcef75e022337a6cf957fc925637b6c65ff8011
Илья <bartenev.ilya@gmail.com>
2021-10-02 17:43:57 +0300

    Установит prop-types и добавит валидацию пропсов для компонентов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import WelcomeScreen from "../welcome-screen/welcome-screen";
+import PropTypes from "prop-types";
 
-const App = () => {
-  return <WelcomeScreen />;
+const App = (props) => {
+  const {filmNames} = props;
+
+  return <WelcomeScreen
+    filmNames={filmNames}
+  />;
+};
+
+App.propTypes = {
+  filmNames: PropTypes.array.isRequired,
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from "react-dom";
 import App from "./components/app/app";
 
 const init = () => {
+  const films = [`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`, `Aviator`];
+
   ReactDOM.render(
-      <App />,
+      <App
+        films={films}
+      />,
       document.querySelector(`#root`)
   );
 };
