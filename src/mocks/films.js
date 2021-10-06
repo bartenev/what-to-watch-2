65f70240d5c7dda1d2822544adacaf3bf03deb73
Илья <bartenev.ilya@gmail.com>
2021-10-06 12:12:10 +0300

    Создает моки для карточки фильма

---
+++ src/mocks/films.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    title: `Fantastic Beasts`,
+    picture: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`
+  },
+  {
+    title: `Bohemian Rhapsody`,
+    picture: `img/bohemian-rhapsody.jpg`
+  },
+  {
+    title: `Macbeth`,
+    picture: `img/macbeth.jpg`
+  },
+  {
+    title: `Aviator`,
+    picture: `img/aviator.jpg`
+  },
+  {
+    title: `We need to talk about Kevin`,
+    picture: `img/we-need-to-talk-about-kevin.jpg`
+  },
+];
