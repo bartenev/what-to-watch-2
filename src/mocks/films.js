c7feaf198073c707276503f4a7a00dcfe005a211
Илья <bartenev.ilya@gmail.com>
2021-10-08 15:28:33 +0300

    Добавляет в моки ссылки на превью с видео

--- src/mocks/films.js
+++ src/mocks/films.js
@@ -1,22 +1,31 @@
 export default [
   {
     title: `Fantastic Beasts`,
-    picture: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`
+    picture: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
   },
   {
     title: `Bohemian Rhapsody`,
-    picture: `img/bohemian-rhapsody.jpg`
+    picture: `img/bohemian-rhapsody.jpg`,
+    preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+
   },
   {
     title: `Macbeth`,
-    picture: `img/macbeth.jpg`
+    picture: `img/macbeth.jpg`,
+    preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+
   },
   {
     title: `Aviator`,
-    picture: `img/aviator.jpg`
+    picture: `img/aviator.jpg`,
+    preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+
   },
   {
     title: `We need to talk about Kevin`,
-    picture: `img/we-need-to-talk-about-kevin.jpg`
+    picture: `img/we-need-to-talk-about-kevin.jpg`,
+    preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+
   },
 ];
