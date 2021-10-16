f559838744c34f5c470eda087e8dbef34e267f1b
Илья <bartenev.ilya@gmail.com>
2021-10-16 22:02:32 +0300

    Убирает таб с отзывами у фильмов, у которых их нет

--- src/mocks/films.js
+++ src/mocks/films.js
@@ -77,7 +77,7 @@ const getRandomArrayOfSomething = (getElement, max, min, array = []) => {
 
 const filmsNames = [`Bohemian Rhapsody`, `Macbeth`, `Aviator`, `We need to talk about Kevin`, `What We Do in the Shadows`,
   `Revenant`, `Johnny English`, `Shutter Island`, `Pulp Fiction`, `No Country for Old Men`, `Snatch`, `Moonrise Kingdom`,
-  `Seven Years in Tibet`, `Midnight Special`, `War of the Worlds`, `Dardjeeling Limited`, `Orlando`, `Mindhunter`, `Midnight Special`];
+  `Seven Years in Tibet`, `War of the Worlds`, `Dardjeeling Limited`, `Orlando`, `Mindhunter`, `Midnight Special`];
 
 const getFilms = () => {
   return filmsNames.map((filmName) => {
