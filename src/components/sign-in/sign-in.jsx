e7e74c79b6fade9b47acb2e38ef31869db060004
Илья <bartenev.ilya@gmail.com>
2021-11-05 21:26:11 +0300

    Делает поля формы входа обязательными для заполнения

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -43,12 +43,12 @@ const SignIn = (props) => {
           <div className="sign-in__fields">
             <div className="sign-in__field">
               <input className="sign-in__input" type="email" placeholder="Email address" name="user-email"
-                id="user-email" ref={emailRef}/>
+                id="user-email" ref={emailRef} required/>
               <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
             </div>
             <div className="sign-in__field">
               <input className="sign-in__input" type="password" placeholder="Password" name="user-password"
-                id="user-password" ref={passwordRef}/>
+                id="user-password" ref={passwordRef} required/>
               <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
             </div>
           </div>
